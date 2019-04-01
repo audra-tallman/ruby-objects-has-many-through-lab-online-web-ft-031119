dda9b989d3debf6d517060ed0db31dbff908cc18
Audra Tallman <audi.tallman@gmail.com>
2019-04-01 23:44:27 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,8 +12,8 @@ class Patient
     @@all 
   end 
   
-  def new_appointment(doctor, date)
-    Appointment.new(self ,doctor, date)
+  def new_appointment(date, doctor)
+    Appointment.new(self, doctor)
   end
   
   def appointments
